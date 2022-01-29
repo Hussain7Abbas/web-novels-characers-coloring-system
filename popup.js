6f5503c9c87f35afe7851456dd76041b0e29e0b6
hussain7abbas <hussain.jazing.21@gmail.com>
2022-01-29 17:07:51 +0300

    fix some bugs

--- popup.js
+++ popup.js
@@ -54,16 +54,53 @@
         bgScript = chrome.extension.getBackgroundPage()
         characters = bgScript.characters;
 
-        var characters_p = `<div id="chars_header" class="_chars_rows"><p class="_p">الاسم</p><p class="_p">اللون</p><p class="_p">الدور</p></div>`
+        char_div.appendChild(createRow({"name":"name", "color":"color", "role":"role"}, "header"))
+        // var characters_p = `<div id="chars_header" class="_chars_rows"><p class="_p">الاسم</p><p class="_p">اللون</p><p class="_p">الدور</p></div>`
         for (const key in characters) {
-
             if (Object.hasOwnProperty.call(characters, key)) {
                 char = characters[key]
-                characters_p += `<div class="_chars_rows" onclick="loadChar('`+char.name+`')"><p class="_p">`+char.name+`</p><p class="_p" style="color: `+char.color+`;">`+char.color+`</p><p class="_p">`+char.role+`</p></div>`
+
+                char_div.appendChild(createRow(char))
+
+                // characters_p += `<div class="_chars_rows" id="`+char.name+`"><p class="_p">`+char.name+`</p><p class="_p" style="color: `+char.color+`;">`+char.color+`</p><p class="_p">`+char.role+`</p></div>`
             }
         }
         console.log(char_div);
-        char_div.innerHTML = characters_p
+        // char_div.innerHTML = characters_p
+
+    }
+
+
+
+    function createRow(columns, type="row") {
+        var row_div = document.createElement('div')
+        if (type=="row"){
+            row_div.className = "_chars_rows"
+            // row_div.addEventListener('click', loadChar(columns.name))
+        }else{
+            row_div.className = "_chars_header _chars_rows"
+        }
+        
+
+        var name_p = document.createElement('p')
+        name_p.appendChild(document.createTextNode(columns.name))
+        name_p.style['color'] = columns.color
+        name_p.className = "_p"
+        row_div.appendChild(name_p)
+
+        var color_p = document.createElement('p')
+        color_p.appendChild(document.createTextNode(columns.color))
+        color_p.style['color'] = columns.color
+        color_p.className = "_p"
+        row_div.appendChild(color_p)
+
+        var role_p = document.createElement('p')
+        role_p.appendChild(document.createTextNode(columns.role))
+        name_p.style['color'] = columns.color
+        role_p.className = "_p"
+        row_div.appendChild(role_p)
+
+        return row_div
 
     }
 
